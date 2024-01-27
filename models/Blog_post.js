56d9daf476b23067a063e8e87e69432e14d9e5af
dark-king-001 <piyushat115@gmail.com>
2024-01-27 12:49:07 +0530

    Blog_post schema added

---
+++ models/Blog_post.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+    blogId: {type:String , required: true},
+    title: {type: String, required:true},
+    body: {type: String, required:true},
+    editors: {type: String, requred: true},
+    tags: [{ type:String }],
+    imageLinks: [{type: String}],
+    relativeLinks: [{type: String}],
+    dateUploaded: {type: Date, default:Date.now()},
+});
+
+const model = mongoose.model("blogs", blogSchema);
+module.exports = model;
